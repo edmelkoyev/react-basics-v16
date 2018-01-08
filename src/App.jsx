51f955808e2c601840b122c540a72a75f9662acf
Edward Melkoyev <edmelkoyev@gmail.com>
2018-01-08 19:07:01 +0300

    modal window  with auto-created div element

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 import './App.css';
 import logo from './logo.svg';
+import Modal from './components/Modal';
 
 export default class App extends Component {
+    state = {
+        isModalOpen: false
+    };
+
+    toggleModal = () => {
+        this.setState(state => ({isModalOpen: !state.isModalOpen}));
+    };
+
     render() {
         return (
             <div className="app">
@@ -14,10 +22,13 @@ export default class App extends Component {
                 </header>
 
                 <main>
-                    {ReactDOM.createPortal(
-                        <h1>Portal</h1>,
-                        document.getElementById('portal')
-                    )}
+                    <button onClick={this.toggleModal}>Open</button>
+                    {this.state.isModalOpen &&
+                        <Modal onClose={this.toggleModal}>
+                            <h1>Modal</h1>
+                        </Modal>
+                    }
+                    
                 </main>
             </div>
         );
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './Modal.css';
 
 export default class Modal extends React.Component {
+    componentWillMount() {
+        this.mRoot = document.createElement('div');
+        document.body.appendChild(this.mRoot);
+    }
+
+    componentWillUnmount() {
+        document.body.removeChild(this.mRoot);
+    }
+
     render() {
-        return (
+        return ReactDOM.createPortal(
             <div className="modal">
-                <button className="modal__close-button">Закрыть</button>
+                <button className="modal__close-button" onClick={this.props.onClose}>Закрыть</button>
                 {this.props.children}
-            </div>
+            </div>,
+            this.mRoot
         );
     }
 }
